1dc8258a5293888d2d17c4ce5a711080331a8d88
Ryan Naddy <rnaddy@corp.acesse.com>
2019-04-01 16:34:50 -0500

    update config for logging

--- config/app.js
+++ config/app.js
@@ -1,9 +1,13 @@
-const { env } = require('@red5/server')
+const { env, storagePath } = require('@red5/server')
 
 /** @type {import('@red5/server').AppSettings} exports */
 module.exports = {
   port: parseInt(env('APP_PORT', '80')),
   name: env('APP_NAME', 'Red5'),
   env: env('APP_ENV', 'production'),
-  https: false
+  https: false,
+  logs: {
+    error: storagePath('logs/error.log'),
+    access: storagePath('logs/access.log')
+  }
 }
